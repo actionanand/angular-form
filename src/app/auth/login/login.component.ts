1e2924b0b31955c125e7842fe2b1ded2030f238a
Anand raja <meetanandns@gmail.com>
2024-10-01 20:10:59 +0000

    ng form

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,52 @@
-import { Component } from '@angular/core';
+import { afterNextRender, Component, DestroyRef, inject, viewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-login',
   standalone: true,
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  minPassLength = 5;
+  private formData = viewChild.required<NgForm>('formEl');
+  private destroyRef = inject(DestroyRef);
+
+  constructor() {
+    afterNextRender(() => {
+      const localFormVal = localStorage.getItem('saved-login-form');
+
+      if (localFormVal) {
+        const savedFormEmail = JSON.parse(localFormVal).email;
+
+        this.formData().setValue({
+          email: savedFormEmail,
+          password: '',
+        });
+      }
+
+      const formSub = this.formData()
+        .valueChanges?.pipe(debounceTime(1000))
+        .subscribe({
+          next: form => {
+            localStorage.setItem('saved-login-form', JSON.stringify({ email: form.email }));
+          },
+        });
+
+      this.destroyRef.onDestroy(() => formSub?.unsubscribe());
+    });
+  }
+
+  onSubmit(formEl: NgForm) {
+    if (formEl.form.invalid) {
+      return;
+    }
+
+    console.log(formEl);
+
+    formEl.form.reset();
+  }
+}
